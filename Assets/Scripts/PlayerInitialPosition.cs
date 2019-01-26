e1d47fa88e1db6780398f7f1d4e6829f386ec179
Luke Howden <32733425+LankyLuke97@users.noreply.github.com>
2019-01-26 22:42:53 +0000

    Add files via upload

---
+++ Assets/Scripts/PlayerInitialPosition.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInitialPosition : MonoBehaviour {
+    public void initialPos(Vector3 position) {
+        transform.position = position;
+    }
+}
