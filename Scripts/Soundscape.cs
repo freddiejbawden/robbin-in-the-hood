1d5950ed01076702e5a5887c0ead54a5a61e9e0b
Harry Howley <hjhowley99@hotmail.co.uk>
2019-01-26 22:31:42 +0000

    Add files via upload

---
+++ Scripts/Soundscape.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soundscape : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] clips;
+    [SerializeField] private AudioClip rumble;
+    int clipIndex = 0;
+    AudioSource source;
+
+    private void Start() {
+        source = transform.GetComponent<AudioSource>();
+        source.PlayOneShot(rumble);
+        StartCoroutine(playPing());
+    }
+
+    public IEnumerator playPing() {
+        while(true) {
+        yield return new WaitForSeconds(Random.Range(0.0f, 2.0f));
+             clipIndex = Random.Range(0, clips.Length);
+             source.PlayOneShot(clips[clipIndex], 1.0f);
+        }
+    }
+}
