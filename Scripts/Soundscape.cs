b889c390f305ef14fd77b4a1752df9366ac28ff7
Harry Howley <hjhowley99@hotmail.co.uk>
2019-01-26 23:58:03 +0000

    added norman osborne vo

--- Scripts/Soundscape.cs
+++ Scripts/Soundscape.cs
@@ -6,6 +6,9 @@ public class Soundscape : MonoBehaviour
 {
     [SerializeField] private AudioClip[] clips;
     [SerializeField] private AudioClip rumble;
+    [SerializeField] private AudioClip avenge_me_normal;
+    [SerializeField] private AudioClip avenge_me_loud;
+    [SerializeField] private AudioClip avenge_me_backwards;
     int clipIndex = 0;
     AudioSource source;
 
@@ -13,13 +16,37 @@ public class Soundscape : MonoBehaviour
         source = transform.GetComponent<AudioSource>();
         source.PlayOneShot(rumble);
         StartCoroutine(playPing());
+        StartCoroutine(playAM_normal());
+        StartCoroutine(playAM_loud());
+        StartCoroutine(playAM_backwards());
     }
 
     public IEnumerator playPing() {
         while(true) {
-        yield return new WaitForSeconds(Random.Range(0.0f, 2.0f));
+        yield return new WaitForSeconds(Random.Range(0.0f, 5.0f));
              clipIndex = Random.Range(0, clips.Length);
              source.PlayOneShot(clips[clipIndex], 1.0f);
+        }
+    }
+
+    public IEnumerator playAM_normal() {
+        while(true) {
+        yield return new WaitForSeconds(Random.Range(5.0f, 10.0f));
+             source.PlayOneShot(avenge_me_normal);
+        }
+    }
+
+    public IEnumerator playAM_loud() {
+        while(true) {
+        yield return new WaitForSeconds(Random.Range(10.0f, 15.0f));
+             source.PlayOneShot(avenge_me_loud);
+        }
+    }
+
+    public IEnumerator playAM_backwards() {
+        while(true) {
+        yield return new WaitForSeconds(Random.Range(10.0f, 15.0f));
+             source.PlayOneShot(avenge_me_backwards);
         }
     }
 }
